b77152f24da92a2ed93e80fd4c4b8401a09c4be9
JimCoder-Dev <jamesholmson88@gmail.com>
2022-07-04 22:52:23 +0100

    Form entry

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -15,24 +15,34 @@ const Meme = () => {
       ...prevMeme,
       randomImage: memesData.data.memes[randomMeme].image,
     }));
+  }
 
-  
+  function handleChange(event){
+    const { name, value } = event.target
+    setMemeImage(prevMeme => ({
+      ...prevMeme,
+      [name]: value
+    }))
   }
 
   return (
     <main>
       <div className="form">
-        <input className="form--input" type="text" placeholder="Top Text" />
-        <input className="form--input" type="text" placeholder="Bottom Text" />
+        <input className="form--input" type="text" placeholder="Top Text" name="topText" value={memeImage.topText} onChange={handleChange}/>
+        <input className="form--input" type="text" placeholder="Bottom Text" name="bottomText" value={memeImage.bottomText} onChange={handleChange}/>
         <button onClick={handleClick} className="form--button">
           Get new meme image 🖼
         </button>
-
-        <img
-          className="meme-image"
-          src={memeImage.randomImage}
-          alt="A meme"
-        />
+        <div className="meme">
+            <img
+            className="meme-image"
+            src={memeImage.randomImage}
+            alt="A meme"
+            />
+            <h2 className="meme--text top">{memeImage.topText}</h2>
+            <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
+        </div>
+        
       </div>
     </main>
   );
